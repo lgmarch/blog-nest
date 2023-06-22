1552f9702eb7dd276b81312f4878329fed68221a
lgmarch <lmarch@yandex.ru>
2023-06-22 22:06:16 +0300

    Add a OpenAPI of the DTO/RDO.

--- project/apps/users/src/app/auth/dto/create-user.dto.ts
+++ project/apps/users/src/app/auth/dto/create-user.dto.ts
@@ -1,7 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User unique address.',
+    example: 'user@user.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User first name.',
+    example: 'Mike',
+  })
   public firstName: string;
+
+  @ApiProperty({
+    description: 'User last name.',
+    example: 'Saida'
+  })
   public lastName: string;
+
+  @ApiProperty({
+    description: 'User password.',
+    example: '123456'
+  })
   public password: string;
+
+  @ApiProperty({
+    description: 'User avatar path.',
+    example: '/images/user.png'
+  })
   public avatar?: string;
 }
--- project/apps/users/src/app/auth/rdo/logged-user.rdo.ts
+++ project/apps/users/src/app/auth/rdo/logged-user.rdo.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class LoggerUserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID.',
+    example: '13'
+  })
   @Expose({name: '_id'})	// поле сущности
   public id: string;
 
+  @ApiProperty({
+    description: 'User unique address.',
+    example: 'user@user.ru'
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'Access token',
+    example: 'klasdku78dfua3k098o'
+  })
   @Expose()
   public accessToken: string;
 }
